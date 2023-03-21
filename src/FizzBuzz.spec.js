246a6190983160e0aa433ea0c207721223a303a6
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-03-20 22:33:01 -0400

    primera prueba retornar 1 - passed

---
+++ src/FizzBuzz.spec.js
@@ -0,0 +1,7 @@
+import fizzbuzz from "./fizzbuzz";
+
+describe("es fizzbuzz", () => {
+  it("deberia retornar 1", () => {
+    expect(fizzbuzz(1)).toEqual(1);
+  });
+});
---
+++ src/fizzbuzz.js
@@ -0,0 +1,5 @@
+function fizzbuzz(num) {
+    return 1;
+  }
+  
+  export default fizzbuzz;
